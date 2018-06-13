f631bd628b32b65c5ca273c43ef98cb7b61e11d2
Zou Wei <zwcloud@hotmail.com>
2018-06-13 13:35:02 +0800

    clean

--- EchoLogger.Server/Program.cs
+++ EchoLogger.Server/Program.cs
@@ -9,30 +9,21 @@ namespace EchoLogger
     {
         public static void Main()
         { 
-            TcpListener server=null;   
+            TcpListener server = null;   
             try
             {
                 // Set the TcpListener on port 13000.
                 Int32 port = 13000;
                 IPAddress localAddr = IPAddress.Parse("127.0.0.1");
-      
-                // TcpListener server = new TcpListener(port);
                 server = new TcpListener(localAddr, port);
 
                 // Start listening for client requests.
                 server.Start();
-         
-                // Buffer for reading data
-                Byte[] bytes = new Byte[256];
-                String data;
 
-                // Enter the listening loop.
+                // the listening loop.
                 while(true)
                 {
                     Console.Write("Waiting for a connection... ");
-        
-                    // Perform a blocking call to accept requests.
-                    // You could also user server.AcceptSocket() here.
                     TcpClient client = server.AcceptTcpClient();            
                     Console.WriteLine("Connected!");
                     NetworkStream stream = client.GetStream();
@@ -44,8 +35,6 @@ namespace EchoLogger
                             Console.WriteLine(line);
                         }
                     }
-         
-                    // Shutdown and end connection
                     client.Close();
                 }
             }
@@ -55,10 +44,8 @@ namespace EchoLogger
             }
             finally
             {
-                // Stop listening for new clients.
                 server.Stop();
             }
-
       
             Console.WriteLine("\nHit enter to continue...");
             Console.Read();
